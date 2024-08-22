4e01c0e0637291130ab10126698989fc86e91d66
rasoul <rasoul_piramoon@yahoo.com>
2024-08-22 10:24:14 +0330

    init

---
+++ MInStepsForTurnOnBulb/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public static class Solution
+{
+    public static int solve(int A)
+    {
+        int minStep = 0;
+        if (A == 1)
+            minStep = 1;
+        else if (A == 2 || A == 3)
+            minStep = 2;
+        else if (A > 3)
+        {
+            int i = 3;
+            while (true)
+            {
+                minStep++;
+                i++;
+                if (i > A)
+                {
+                    minStep++;
+                    break;
+                }
+                minStep++;
+                if ((i + 5) > A)
+                {
+                    if ((A - i) == 3)
+                        minStep++;
+                    else if ((A - i) == 4)
+                        minStep = minStep + 2;
+                    break;
+                }
+                i = i + 5;
+            }
+        }
+        return minStep;
+    }
+}
